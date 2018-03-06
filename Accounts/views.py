559b77c33189a1ffa9f09485ae5bfffb9b6968e7
naeem008 <zannatnaeem3@gmail.com>
2018-03-07 00:38:35 +0600

    Added login functionality

--- Accounts/views.py
+++ Accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from .models import Staff, Bank_statement, Salary_info, User
+
 
 # Create your views here.
+
+def getLogin(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        auth = authenticate(request, username=username, password=password)
+        if auth is not None:
+            login(request, username, password)
+            return redirect("index")
+        else:
+            return render(request, "login.html")
+    else:
+        return render(request, "login.html")
